5cbaa94176c16f413881f47d5b428a2d1254b6db
Rafael Perez <rafael.r.perez@oracle.com>
2021-12-03 15:59:29 -0600

    mob next [ci-skip] [ci skip] [skip ci]

--- src/util/NameNormalizer.ts
+++ src/util/NameNormalizer.ts
@@ -9,8 +9,13 @@ class NameNormalizer {
   normalize(name: string): string {
     name = name.trim()
     const nameParts = name.split(' ')
-    const reversed = nameParts.reverse()
-    return reversed.join(', ')
+    if (nameParts.length == 2) {
+      const reversed = nameParts.reverse()
+      return reversed.join(', ')
+    } else if (nameParts.length == 3){
+      
+    }
+    return name;
   }
 }
 
